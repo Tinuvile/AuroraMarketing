c0de4899b34ea230b53127e48df80b889bb1b03d
Tinuvile <3145593233@qq.com>
2025-11-25 21:46:52 +0800

    fix
    Signed-off-by: Tinuvile <3145593233@qq.com>

--- AuroraMarketing-app/src/test/java/com/tinuvile/test/domain/RaffleStrategyTest.java
+++ AuroraMarketing-app/src/test/java/com/tinuvile/test/domain/RaffleStrategyTest.java
@@ -5,12 +5,15 @@ import com.alibaba.fastjson.JSON;
 import com.tinuvile.domain.strategy.model.entity.RaffleAwardEntity;
 import com.tinuvile.domain.strategy.model.entity.RaffleFactorEntity;
 import com.tinuvile.domain.strategy.service.IRaffleStrategy;
+import com.tinuvile.domain.strategy.service.armory.IStrategyArmory;
+import com.tinuvile.domain.strategy.service.armory.StrategyArmoryDispatch;
 import com.tinuvile.domain.strategy.service.rule.impl.RuleWeightLogicFilter;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.util.ReflectionTestUtils;
@@ -31,10 +34,15 @@ public class RaffleStrategyTest {
     private IRaffleStrategy raffleStrategy;
     @Resource
     private RuleWeightLogicFilter ruleWeightLogicFilter;
+    @Resource
+    private IStrategyArmory strategyArmory;
 
     @Before
     public void setUp() {
         ReflectionTestUtils.setField(ruleWeightLogicFilter, "userScore", 40500L);
+
+        boolean success = strategyArmory.assembleLotteryStrategy(10001L);
+        log.info("策略装配初始化结果:{}", success);
     }
 
     @Test
