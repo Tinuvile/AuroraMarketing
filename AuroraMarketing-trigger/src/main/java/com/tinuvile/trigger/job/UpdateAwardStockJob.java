d3a2927e8e783b4b8d9ec37c27236b3dbb3c4548
Tinuvile <3145593233@qq.com>
2025-12-04 20:37:42 +0800

    fix
    Signed-off-by: Tinuvile <3145593233@qq.com>

--- AuroraMarketing-trigger/src/main/java/com/tinuvile/trigger/job/UpdateAwardStockJob.java
+++ AuroraMarketing-trigger/src/main/java/com/tinuvile/trigger/job/UpdateAwardStockJob.java
@@ -1,10 +1,8 @@
 package com.tinuvile.trigger.job;
 
-
 import com.tinuvile.domain.strategy.model.valobj.StrategyAwardStockKeyVO;
 import com.tinuvile.domain.strategy.service.IRaffleStock;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -13,12 +11,11 @@ import javax.annotation.Resource;
 /**
  * @author Tinuvile
  * @description 更新奖品库存任务
- * 使用redis更新缓存库存，异步队列更新数据库，避免压力打到数据库
+ *              使用redis更新缓存库存，异步队列更新数据库，避免压力打到数据库
  * @since 2025/12/2
  */
 @Slf4j
 @Component
-@Profile("!test")
 public class UpdateAwardStockJob {
 
     @Resource
@@ -29,10 +26,12 @@ public class UpdateAwardStockJob {
         try {
             log.info("定时任务 - 更新奖品消耗库存开始");
             StrategyAwardStockKeyVO strategyAwardStockKeyVO = raffleStock.takeQueueValue();
-            if (null == strategyAwardStockKeyVO) return;
+            if (null == strategyAwardStockKeyVO)
+                return;
             log.info("定时任务 - 更新奖品消耗库存 strategyId:{} awardId:{}",
                     strategyAwardStockKeyVO.getStrategyId(), strategyAwardStockKeyVO.getAwardId());
-            raffleStock.updateStrategyAwardStock(strategyAwardStockKeyVO.getStrategyId(), strategyAwardStockKeyVO.getAwardId());
+            raffleStock.updateStrategyAwardStock(strategyAwardStockKeyVO.getStrategyId(),
+                    strategyAwardStockKeyVO.getAwardId());
         } catch (Exception e) {
             log.error("定时任务 - 更新奖品消耗库存失败", e);
         }
