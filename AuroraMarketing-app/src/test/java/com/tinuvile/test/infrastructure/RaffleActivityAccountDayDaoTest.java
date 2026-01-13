b7acddc050889fbb32e15c7fe33f9fabdb75884b
Tinuvile <3145593233@qq.com>
2026-01-13 19:09:18 +0800

    dev
    Signed-off-by: Tinuvile <3145593233@qq.com>

---
+++ AuroraMarketing-app/src/test/java/com/tinuvile/test/infrastructure/RaffleActivityAccountDayDaoTest.java
@@ -0,0 +1,37 @@
+package com.tinuvile.test.infrastructure;
+
+
+import com.tinuvile.infrastructure.persistent.dao.IRaffleActivityAccountDayDao;
+import com.tinuvile.infrastructure.persistent.po.RaffleActivityAccountDay;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * @author Tinuvile
+ * @description 活动日账户Dao
+ * @since 2026/1/13
+ */
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RaffleActivityAccountDayDaoTest {
+
+    @Resource
+    private IRaffleActivityAccountDayDao raffleActivityAccountDayDao;
+
+    @Test
+    public void test_queryRaffleActivityAccountDayPartakeCount() {
+        RaffleActivityAccountDay raffleActivityAccountDay = new RaffleActivityAccountDay();
+        raffleActivityAccountDay.setActivityId(100301L);
+        raffleActivityAccountDay.setUserId("tinuvile");
+        raffleActivityAccountDay.setDay(raffleActivityAccountDay.currentDay());
+        Integer dayPartakeCount = raffleActivityAccountDayDao.queryRaffleActivityAccountDayPartakeCount(raffleActivityAccountDay);
+        log.info("测试结果:{}", dayPartakeCount);
+    }
+
+}
