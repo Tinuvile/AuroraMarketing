99e181d90e48595c7402629b9c55e7089e1db280
Tinuvile <3145593233@qq.com>
2026-01-29 23:45:20 +0800

    fix
    Signed-off-by: Tinuvile <3145593233@qq.com>

--- AuroraMarketing-domain/src/main/java/com/tinuvile/domain/activity/service/product/RaffleActivitySkuProductService.java
+++ AuroraMarketing-domain/src/main/java/com/tinuvile/domain/activity/service/product/RaffleActivitySkuProductService.java
@@ -4,6 +4,7 @@ package com.tinuvile.domain.activity.service.product;
 import com.tinuvile.domain.activity.model.entity.SkuProductEntity;
 import com.tinuvile.domain.activity.repository.IActivityRepository;
 import com.tinuvile.domain.activity.service.IRaffleActivitySkuProductService;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.List;
  * @description sku商品服务
  * @since 2026/1/29
  */
+@Service
 public class RaffleActivitySkuProductService implements IRaffleActivitySkuProductService {
 
     @Resource
